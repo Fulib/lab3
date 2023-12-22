346fb1a08d6cc64d823dd1c5a885f8cf6209640d
fulib <1641477361@qq.com>
2023-12-22 16:57:59 +0800

    second

---
+++ lab3-2.16alpha93/AnkiDroid/src/main/java/com/ichi2/compat/CompatV33.kt
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2022 Ashish Yadav <mailtoashish693@gmail.com>
+ *  Copyright (c) 2022 David Allison <davidallisongithub@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software
+ *  Foundation; either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with
+ *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.ichi2.compat
+
+import android.annotation.TargetApi
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.util.SparseArray
+import java.io.Serializable
+
+@TargetApi(33)
+open class CompatV33 : CompatV31(), Compat {
+    override fun <T : Parcelable> getParcelableArrayList(bundle: Bundle, key: String, clazz: Class<T>): ArrayList<T>? {
+        return bundle.getParcelableArrayList(key, clazz)
+    }
+
+    override fun <T : Serializable?> getSerializableExtra(intent: Intent, name: String, className: Class<T>): T? {
+        return intent.getSerializableExtra(name, className)
+    }
+
+    override fun <T : Parcelable?> getParcelableExtra(intent: Intent, name: String, clazz: Class<T>): T? {
+        return intent.getParcelableExtra(name, clazz)
+    }
+
+    override fun <T> readSparseArray(
+        parcel: Parcel,
+        loader: ClassLoader,
+        clazz: Class<T>
+    ): SparseArray<T>? {
+        return parcel.readSparseArray(loader, clazz)
+    }
+}
